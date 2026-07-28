e1f4c3ae3c3290426f64628f3be302f9de80d5fe
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-07-28 16:28:55 -0300

    Cálculo do volume de uma esfera de acordo com PI.

---
+++ Estudos Básicos/VolumeEsfera.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+    double pi=3.14159, R, VOLUME;
+    
+    scanf("%lf", &R);
+    VOLUME = (4.0/3) * pi * pow(R, 3);
+    
+    printf("VOLUME = %.3lf\n", VOLUME);
+ 
+    return 0;
+}
