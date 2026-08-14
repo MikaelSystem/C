a0102335fb542103a3a3741450f19f5b2abb6b6f
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-08-14 17:28:16 -0300

    Update 3305_BisposDominantes.cpp

--- C++ Exercises/3305_BisposDominantes.cpp
+++ C++ Exercises/3305_BisposDominantes.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int bispos=0, X=0 ,Y=0;
+    int bispos=0, tabuleiro, X=0 ,Y=0;
 
     map<pair<int,int>int> tabuleiro;
 
@@ -17,8 +17,14 @@ int main() {
         }
     }
 
-    for (auto item:tabuleiro){
-        
+    for (auto [coordenada, valor]:tabuleiro){
+        int X=casa.first;
+        int Y=casa.second;
+
+        if (X==0){
+            if (Y%2==0 && valor == 1) casasbrancas++;
+            else casaspretas++;
+        }
     }
         
 
