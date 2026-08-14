9793b9fa3243e68aa728c5ad491863b05fd283f9
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-08-14 16:54:33 -0300

    PENDENTE, descobrir bispos que compartilham mesma diagonal em um tabuleiro de xadrez.
    Implement basic structure for a program to handle dominant bishops on a chessboard.

---
+++ C++ Exercises/3305_BisposDominantes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int bispos=0, X=0 ,Y=0;
+
+    map<pair<int,int>int> tabuleiro;
+
+    cin >> bispos;
+
+    for (int i=1;i<=bispos;i++){
+        if (cin >> X >> Y){
+            tabuleiro[{X,Y}] = 1
+        }
+    }
+
+    for (auto item:tabuleiro){
+        
+    }
+        
+
+    return 0;
+}
