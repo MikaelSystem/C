6982261a8ab570943d887ae27778f1f387f70ac0
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-08-14 12:45:04 -0300

    Problema estruturado da maquina de café.
    Aqui eu testo os andares possiveis e onde possivelmente a maquina estaria trazendo a minutagem mais eficiente.

--- C++ Exercises/2670_MaquinaCafe.cpp
+++ C++ Exercises/2670_MaquinaCafe.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int A1,A2,A3,R;
+    int A1=0 ,A2=0, A3=0, andar1=0, andar2=0, andar3=0;
     cin >> A1 >> A2 >> A3;
 
-    if ((A1*2)+(A3*2) < (A2*4)){
-        cout << (A1*2) + (A3*2);
-    }
+    andar1 = ((A2*2)+(A3*4));
+    andar2 = ((A1*2)+(A3*2));
+    andar3 = ((A2*2)+(A1*4));
 
+    int R = min({andar1,andar2,andar3});
+
+    cout << R << "\n";
+    
     return 0;
 }
