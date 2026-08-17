107ac32ac6009c2cc4d153ab1a49ee3e2cf589d2
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-08-17 11:36:42 -0300

    Bispos dominantes ainda nao terminado

--- C++ Exercises/3305_BisposDominantes.cpp
+++ C++ Exercises/3305_BisposDominantes.cpp
@@ -5,28 +5,36 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int bispos=0, tabuleiro, X=0 ,Y=0;
+    int bispos=0, X=0 ,Y=0;
+    int casasbrancas=0, casaspretas=0;
 
-    map<pair<int,int>int> tabuleiro;
+    map<pair<int,int>,int> tabuleiro;
 
     cin >> bispos;
 
     for (int i=1;i<=bispos;i++){
         if (cin >> X >> Y){
-            tabuleiro[{X,Y}] = 1
+            tabuleiro[{X,Y}] = 1;
         }
     }
 
-    for (auto [coordenada, valor]:tabuleiro){
-        int X=casa.first;
-        int Y=casa.second;
-
-        if (X==0){
-            if (Y%2==0 && valor == 1) casasbrancas++;
-            else casaspretas++;
-        }
+    for (auto [casa, valor] : tabuleiro){
+        X = casa.first;
+        Y = casa.second;
+   
+        if (X==Y && valor == 1) casasbrancas++;
+        else if (X!=Y && valor == 1) casaspretas++;
     }
-        
+
+    if (casasbrancas > 1) casasbrancas=0;
+    if (casaspretas > 1) casaspretas=0;
+
+    if (casasbrancas == 1 && casaspretas == 1){
+        cout << casasbrancas + casaspretas << "\n";
+    }
+    else if (casasbrancas == 1 && casaspretas == 0) cout << casasbrancas << "\n";
+    else if (casasbrancas == 0 && casaspretas == 1) cout << casaspretas << "\n";
+    else cout << casasbrancas + casaspretas << "\n";
 
     return 0;
 }
