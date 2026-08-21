4420ccbae35e1ab7c48f8839ab0d610714148343
Mikael Oliveira <Mikaelroblox076@gmail.com>
2026-08-21 20:41:44 -0300

    Clean up code and fix vector initialization
    Removed unnecessary blank lines and fixed vector initialization.

---
+++ C++ Exercises/1034_FestivalEstatuas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+ 
+int main() {
+    int T=0,MI=0;MF=0;
+    
+    vector<int> MDS = {1,5,10.15};
+    
+    for (int i=0; i<T; i++){
+        cin << MI << MF;
+    }
+
+    return 0;
+}
